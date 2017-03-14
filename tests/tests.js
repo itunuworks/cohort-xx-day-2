3e52cbc957201c2a414fb87a8adf2e3a1ca89157
Itunu Fatoki <itunuworks@gmail.com>
2017-03-14 16:03:54 +0100

    Successfully merged reverseStringTests.js and wordsTest.js to tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,5 @@
 var words = require('../apps/andelabs.js').words;
+var reverseString = require('../apps/andelabs.js').reverseString;
 
 describe("words()", function() {
   it("counts one word", function() {
@@ -55,4 +56,48 @@ describe("words()", function() {
     var expectedCounts = { reserved: 1, words : 1, like :1,  prototype: 1, and : 1, toString: 1,  "ok?": 1};
     expect(words("reserved words like prototype and toString ok?")).toEqual(expectedCounts);
   });
+});
+
+
+describe("Produce the reverse order of a word: ", function() {
+  describe("Case for en empty string", function() {
+
+    it("should return null for empty string", function() {
+      expect(reverseString('')).toEqual(null);
+    });
+
+  });
+
+  describe("Case for palindromes", function() {
+
+    it("should return true for `anna`", function() {
+      expect(reverseString('anna')).toEqual(true);
+    });
+
+    it("should return true for `NaN`", function() {
+      expect(reverseString('NaN')).toEqual(true);
+    });
+
+    it("should return true for `civic`", function() {
+      expect(reverseString('civic')).toEqual(true);
+    });
+
+  });
+
+  describe("Case for normal words", function() {
+
+    it("should return `skoob` for `books`", function() {
+      expect(reverseString('books')).toEqual('skoob');
+    });
+
+    it("should return `nomolos` for `solomon`", function() {
+      expect(reverseString('solomon')).toEqual('nomolos');
+    });
+
+    it("should return `csim` for `misc`", function() {
+      expect(reverseString('misc')).toEqual('csim');
+    });
+
+  });
+
 });
