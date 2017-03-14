ab771a2127296a4096fe9afa63bc59b315dab839
Itunu Fatoki <itunuworks@gmail.com>
2017-03-14 15:28:14 +0100

    Implemented first pass of urlshortening app

--- apps/index.js
+++ apps/index.js
@@ -2,15 +2,20 @@
 
 var request = require('request');
 var key = 'AIzaSyDAHnquUK1gDOTDWE8TZDn9YM6CEP6WSrM';
-var shortUrl = 'http://goo.gl/fbsS';
-request("https://www.googleapis.com/urlshortener/v1/url?key=" + key + "&shortUrl=" + shortUrl, function(error, response, body) {
-  //console.log(body);
-  var bodyObject = JSON.parse(body);
-  if (bodyObject.error === undefined){
-	console.log(bodyObject.longUrl);
-	}
-  else{
-    console.log(bodyObject.error.code);
-    console.log(bodyObject.error.message);
-	}
-});
+
+function getLongUrl(shortUrl){
+
+	request("https://www.googleapis.com/urlshortener/v1/url?key=" + key + "&shortUrl=" + shortUrl, function(error, response, body) {
+	  	//console.log(body);
+		var bodyObject = JSON.parse(body);
+		if (bodyObject.error === undefined){
+			console.log(bodyObject.longUrl);
+		}
+	   	else{
+	    	console.log(bodyObject.error.code + '\n' + bodyObject.error.message);
+		}
+
+	})
+}
+
+getLongUrl('http://goo.gl/BzpZ54');
